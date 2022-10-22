1ff13d1f0dcff07425bc4309857341994f2b969c
nikaslan <96795247+nikaslan@users.noreply.github.com>
2022-10-22 10:35:27 +0300

    Update Program.cs

--- HomeWork9.4/Program.cs
+++ HomeWork9.4/Program.cs
@@ -33,7 +33,7 @@ namespace HomeWork9._4
         static async void BotClientRun()
         {
             string token = System.IO.File.ReadAllText(@"C:\Users\User\source\repos\TG_bots\mytemptestbot2022_bot_token.txt");
-            //5248948021:AAEbCl9bCQyVLM1rxy8TJQiJBkkfzg9OBAY
+            //TJQiJBkkfzg9OBAY524894yVLM1rxy88021:AAEbCl9bCQ
 
             botClient = new TelegramBotClient(token);
 
